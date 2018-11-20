8bc1503b5422bf0b80dd4b27dc50a9c4e2b734f0
henipatel8 <henipatel8@github.com>
2018-11-20 13:42:55 -0500

    dataviz

--- dataviz.py
+++ dataviz.py
@@ -17,6 +17,11 @@ with open("data/googeplaystore.csv") as csvfile:
 			categories.append(row)
 			line_count += 1
 		else:
+			# collect the ratings info
+			ratingsData = row[2];
+			ratingsData = ratingsData.replace("Nan", "0")
+			ratings.append(float(ratingsData))
+
 			# print('collect the rest of the data')
 			installData = row[5]
 			instalData = installData.replace("," "")
@@ -25,5 +30,30 @@ with open("data/googeplaystore.csv") as csvfile:
 			line_count += 1
 
 print('processed', line_count, "rows of data")
-print('first line:', installs[0])
-print('last line:', installs[-1])
+print('first line:', ratings[0])
+print('last line:', ratings[-1])
+
+np_ratings = np.array(ratings)
+
+popular_apps = np_ratings > 4
+pop_pct = (np_ratings[popular_apps]) / len(np_ratings) * 100
+print(pop_pct)
+
+popular_apps = np_ratings > 2
+not_pop_pct = (np_ratings[unpopular_apps]) / len(np_ratings) * 100
+print(not_pop_pct)
+
+mid_apps = 100 - (pop_pct + not_pop_pct)
+
+# now we can plot stuff!
+labels = "Sucks, Meh, Love it!"
+sizes = [not_pop_pct, mid_apps, pop_pct]
+colors = ['yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0.1, 0.15)
+
+plt.pie(sizes, explode=explode, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+
+plt.legend(labels, loc=1)
+plt.title("Do We Love Our Apps")
+plt.xlabel
